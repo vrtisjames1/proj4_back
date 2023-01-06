8e6c893342d1b498e25a52cae6d854b2edf1f020
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2023-01-06 15:18:45 -0500

    asasdasdasd

--- proj4_back/routing.py
+++ proj4_back/routing.py
@@ -1,5 +1,5 @@
 import os
-import channels.asgi
+import proj4_back.asgi
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj4_back.settings")
 channel_layer = proj4_back.asgi.get_channel_layer()
