3cbf83f9a1e5185dd2a171d1cad233774ac46774
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2023-01-06 15:29:52 -0500

    asdasd

--- proj4_back/settings.py
+++ proj4_back/settings.py
@@ -88,8 +88,7 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        },
-        "ROUTING": "proj4_back.asgi.URLRouter",
+        }
     },
 }
 # Database
